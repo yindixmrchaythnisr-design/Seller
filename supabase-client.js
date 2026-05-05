add2434fe8c95498e29cf14ef7020567f865c0b4
yindixmrchaythnisr-design <yindixmrchaythnisr@gmail.com>
2026-05-05 11:33:49 +0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,9 +1,9 @@
-// Replace these with your actual Supabase project details later
-const SUPABASE_URL = 'https://dagodjtvsurrsndnryax.supabase.co/rest/v1/';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_diDckZEjvFtSYE6VH_AxiA_aSB_wz_w';
 
-// Initialize the Supabase client
-// The 'window.supabase' object comes from the script tag we will put in the HTML files
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = 'https://dagodjtvsurrsndnryax.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_diDckZEjvFtSYE6VH_AxiA_aSB_wz_w';
+
+// We name it 'supabaseApp' and attach it to the window so it doesn't conflict with the library
+window.supabaseApp = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 console.log("Supabase connected successfully!");
+
