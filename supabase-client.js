cef3fd95a06ee53bea2e251d73c6218c5f8970a4
yindixmrchaythnisr-design <yindixmrchaythnisr@gmail.com>
2026-05-05 10:26:05 +0700

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // Replace these with your actual Supabase project details later
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_LONG_ANON_KEY';
+const SUPABASE_URL = 'https://dagodjtvsurrsndnryax.supabase.co/rest/v1/';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_diDckZEjvFtSYE6VH_AxiA_aSB_wz_w';
 
 // Initialize the Supabase client
 // The 'window.supabase' object comes from the script tag we will put in the HTML files
